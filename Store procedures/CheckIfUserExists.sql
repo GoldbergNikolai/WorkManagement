f40f5b4f9ef01f5ef28f4a65dfadf7c1f91832de
GoldbergNikolai <GoldbergNikolai@users.noreply.github.com>
2022-02-15 23:47:28 +0200

    Update CheckIfUserExists.sql

--- Store procedures/CheckIfUserExists.sql
+++ Store procedures/CheckIfUserExists.sql
@@ -1,21 +1,14 @@
 
-ALTER PROCEDURE CheckIfUserExists
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[CheckIfUserExists]
  @PhoneNumber VARCHAR(20)
 AS
 BEGIN
-	DECLARE @UserPhoneNumber VARCHAR(20) = NULL,
-		    @UserActive INT = 0
-	SELECT @UserPhoneNumber = PhoneNumber, @UserActive = Active FROM Users WHERE PhoneNumber = @PhoneNumber
-	
-	IF @UserPhoneNumber IS NOT NULL
-	BEGIN
-		 SELECT CASE
-					WHEN @UserActive = 1 THEN 1
-					ELSE 0
-				END
-	END
-	ELSE
-	BEGIN
-		SELECT -1
-	END
+	SELECT * 
+	FROM Users
+	WHERE PhoneNumber = @PhoneNumber 
 END
