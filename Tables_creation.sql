a5fd9f43e45833db04c5923cc473aefa469179ba
base0770 <48102845+base0770@users.noreply.github.com>
2022-02-15 15:16:27 +0200

    Add files via upload

---
+++ Tables_creation.sql
@@ -0,0 +1,47 @@
+--create table PhoneNumbers
+--(
+--	 PhoneNumber VARCHAR(20)--(Login)
+--	,Active bit
+--	 primary key (PhoneNumber)
+--)
+
+--create table Users
+--(
+--	 UserId INT IDENTITY
+--	,PhoneNumber VARCHAR(20)--(Login)
+--	,Active BIT
+--	,FirstName NVARCHAR(100)
+--	,LastName NVARCHAR(100)
+--	primary key (UserId),
+--  --foreign key (PhoneNumber) REFERENCES PhoneNumbers(PhoneNumber)
+--)
+
+--create table ToolCategories
+--(
+--	ToolCategoryId INT IDENTITY,
+--	Category VARCHAR(100),
+--	primary key (ToolCategoryId)
+--)
+
+--create table Tools
+--(
+--	ToolId INT IDENTITY,
+--	CategoryId INT, 
+--	ToolName NVARCHAR(1000),
+--	primary key (ToolId),
+--	foreign key (CategoryId) REFERENCES ToolCategories(ToolCategoryId)
+--)
+
+CREATE table Invocations
+(
+	WorkOrderNumber INT,
+	InvocationState VARCHAR(100),
+	ToolId INT,
+	UserId INT, 
+	Category VARCHAR(100),
+	Reason NVARCHAR(1000),
+	ToolName NVARCHAR(1000),
+	InvocationDescription NVARCHAR(4000),
+	primary key (WorkOrderNumber),
+	foreign key (UserId) REFERENCES [dbo].Users(UserId)
+)
