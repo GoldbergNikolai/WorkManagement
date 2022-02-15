329e673e5a06731bc5f8a3f1d571fe0bccf3fead
GoldbergNikolai <GoldbergNikolai@users.noreply.github.com>
2022-02-15 22:45:10 +0200

    Update UpdateUser.sql

--- Store procedures/UpdateUser.sql
+++ Store procedures/UpdateUser.sql
@@ -1,5 +1,10 @@
 
-ALTER PROCEDURE UpdateUser
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[UpdateUser]
   @PhoneNumber VARCHAR(20)
  ,@FirstName NVARCHAR(100)
  ,@LastName NVARCHAR(100)
@@ -8,13 +13,16 @@ BEGIN
 	IF EXISTS (SELECT 1 FROM Users WHERE PhoneNumber = @PhoneNumber)
 	BEGIN
 		UPDATE Users
-		SET FirstName = @FirstName
-		   ,LastName = @LastName
+		SET FirstName = CASE
+							WHEN FirstName <> @FirstName THEN @FirstName
+							ELSE FirstName
+						END
+		   ,LastName = CASE
+						   WHEN LastName <> @LastName THEN @LastName
+						   ELSE LastName
+					   END
+		 WHERE PhoneNumber = @PhoneNumber
 
-		SELECT 1
-	END
-	ELSE
-	BEGIN
-		SELECT 0
+		SELECT SCOPE_IDENTITY()
 	END
 END
