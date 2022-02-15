0e6914dbeb777b1b24a49e144395f0051bb4df32
GoldbergNikolai <GoldbergNikolai@users.noreply.github.com>
2022-02-15 22:24:54 +0200

    Update GetUsers.sql

--- Store procedures/GetUsers.sql
+++ Store procedures/GetUsers.sql
@@ -1,3 +1,4 @@
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -6,12 +7,13 @@ ALTER PROCEDURE [dbo].[GetUsers]
 	@UserId INT = NULL,
 	@PhoneNumber VARCHAR(20) = NULL,
 	@Active BIT = NULL,
-	@IsAdmin BIT = NULL
+	@IsAdmin BIT = NULL,
+	@Top INT = NULL
 AS 
 BEGIN
 	IF (@UserId IS NOT NULL)
 	BEGIN 
-		IF  EXISTS (SELECT * FROM Users WHERE UserId = @UserId)
+		IF  EXISTS (SELECT 1 FROM Users WHERE UserId = @UserId)
 		BEGIN
 			SELECT * FROM Users WHERE UserId = @UserId 
 			AND Active = CASE
@@ -26,9 +28,17 @@ BEGIN
 	END
 	ELSE IF (@PhoneNumber IS NOT NULL)
 	BEGIN
-		IF EXISTS (SELECT * FROM Users WHERE PhoneNumber LIKE '%' + @PhoneNumber + '%')
+		DECLARE @UsersCount INT
+		SELECT @UsersCount = COUNT(1) FROM Users WHERE PhoneNumber LIKE '%' + @PhoneNumber + '%'
+
+		IF @UsersCount > 0
 		BEGIN 
-			SELECT * FROM Users WHERE PhoneNumber LIKE '%' + @PhoneNumber + '%'
+			SET @Top = CASE 
+						   WHEN @Top IS NOT NULL THEN @Top 
+						   ELSE @UsersCount 
+					   END
+
+			SELECT TOP (@Top) * FROM Users WHERE PhoneNumber LIKE '%' + @PhoneNumber + '%'
 			AND Active = CASE
 						 WHEN @Active IS NOT NULL THEN @Active
 						 ELSE Active
