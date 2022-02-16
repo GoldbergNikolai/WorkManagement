0d6cac2376b1a6b087bb43fe46a224721e71fa9d
base0770 <48102845+base0770@users.noreply.github.com>
2022-02-16 13:56:19 +0200

    Update UpdateUserActivationState.sql

--- Store procedures/UpdateUserActivationState.sql
+++ Store procedures/UpdateUserActivationState.sql
@@ -3,6 +3,13 @@ ALTER PROCEDURE UpdateUserActivationState
  @UserId INT
 ,@Active BIT
 AS
+
+--Procedure will update a certain users active state from active to inactive and vice versa
+--Procedure can only be executed by admins
+--input: UserId, Active
+--output: 1- Active state altered successfully
+-- 	  0- UserId was no found
+
 BEGIN
 	DECLARE @UserToAlter INT = NULL
 	SELECT @UserToAlter = 1 FROM Users WHERE UserId = @UserId 
