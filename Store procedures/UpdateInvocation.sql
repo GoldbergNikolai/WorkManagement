4b209f57a141ef3a88f78041330b1d96d932f17c
base0770 <48102845+base0770@users.noreply.github.com>
2022-02-16 13:39:55 +0200

    Update UpdateInvocation.sql

--- Store procedures/UpdateInvocation.sql
+++ Store procedures/UpdateInvocation.sql
@@ -3,7 +3,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
+--PROCEDURE NEEDS TO BE TESTED
 --Procedure will update the Invocation status and Invocation Description fields on a certain chosen WorkOrderNumber(פק"ע)
 --Procedure will add the new description to the the old description creating a log of all necessary data
 --Procedure will be executed by Users in order to give current status on certain tool
@@ -25,12 +25,12 @@ BEGIN
 	
 	UPDATE Invocations 
 	SET InvocationDescription = @InvocationDescriptionToAlter 
-	   ,Updated = GETDATE(), InvocationState = @InvocationState --LAST LINE ("InvocationState = @InvocationState") WAS ADDED IN GIT AND WASNT TESTED, TESTING TO BE DONE
+	   ,Updated = GETDATE(), InvocationState = @InvocationState --LAST LINE WAS ADDED IN GIT AND WASNT TESTED, TESTING TO BE DONE
 	WHERE WorkOrderNumber = @WorkOrderNumber
 
 	IF ((SELECT InvocationDescription FROM Invocations WHERE WorkOrderNumber = @WorkOrderNumber) = @InvocationDescriptionToAlter)
 	BEGIN 
-		IF ((SELECT InvocationState FROM Invocations WHERE WorkOrderNumber = @WorkOrderNumber) = @InvocationStateToAlter)
+		IF ((SELECT InvocationState FROM Invocations WHERE WorkOrderNumber = @WorkOrderNumber) = @InvocationStateToAlter) --THIS SECTION WAS ALSO ADDED IN GIT SO THIS NEEDS TO GET TESTED ASWELL
 		BEGIN
 			SELECT 1
 		END
