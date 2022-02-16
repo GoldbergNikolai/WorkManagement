c584d20de685e14d8ecf9331058fd11b77dbb609
base0770 <48102845+base0770@users.noreply.github.com>
2022-02-16 09:37:54 +0200

    Update CheckIfUserExists.sql

--- Store procedures/CheckIfUserExists.sql
+++ Store procedures/CheckIfUserExists.sql
@@ -4,6 +4,11 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+--Procedure will recieve a phone number and will return a user with a matching phone number
+--Procedure will be used in login (No passwords as of now, idf login api will be implemented later)
+--input: PhoneNumber
+--output: UserId, PhoneNumber, Active, FirstName, LastName, IsAdmin.
+
 ALTER PROCEDURE [dbo].[CheckIfUserExists]
  @PhoneNumber VARCHAR(20)
 AS
